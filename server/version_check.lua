dfd31f67d61457d876e4922ac1cc55eec175734f
Mathewgamingg <mathew.gaming1@gmail.com>
2026-01-03 06:00:54 +0100

    ůprava version check

--- server/version_check.lua
+++ server/version_check.lua
@@ -1,24 +1,41 @@
 Citizen.CreateThread(function()
     Citizen.Wait(5000)
+    
     local function ToNumber(str)
-        return tonumber(str)
+        if not str then return 0 end
+        return tonumber(str:gsub('%.', '')) or 0
     end
+    
     local resourceName = GetCurrentResourceName()
     local currentVersion = GetResourceMetadata(resourceName, 'version', 0)
-    PerformHttpRequest('https://raw.githubusercontent.com/Mathewgamingg/MG_AssetsVersions/master/'..resourceName..'.txt',function(error, result, headers)
-        if not result then 
-            return print('^1The version check failed, github is down.^0') 
+    
+    PerformHttpRequest('https://raw.githubusercontent.com/Mathewgamingg/MG_AssetsVersions/master/'..resourceName..'.txt', function(error, result, headers)
+        -- Kontrola, zda požadavek proběhl v pořádku (kód 200)
+        if error ~= 200 or not result then 
+            return print('^1['..resourceName..'] Kontrola verze selhala (GitHub nedostupný nebo soubor nenalezen).^0') 
         end
-        local result = json.decode(result:sub(1, -2))
-        if ToNumber(result.version:gsub('%.', '')) > ToNumber(currentVersion:gsub('%.', '')) then
-            local symbols = '^9'
-            for cd = 1, 26+#resourceName do
-                symbols = symbols..'-'
+
+        -- Bezpečné dekódování JSONu
+        local success, data = pcall(json.decode, result)
+        
+        if success and data and data.version then
+            if ToNumber(data.version) > ToNumber(currentVersion) then
+                local symbols = '^9'
+                for cd = 1, 26+#resourceName do
+                    symbols = symbols..'-'
+                end
+                symbols = symbols..'^0'
+                
+                print(symbols)
+                print('^3['..resourceName..'] - Je k dispozici nová aktualizace!^0')
+                print('Současná verze: ^1'..currentVersion..'^0')
+                print('Nová verze: ^2'..data.version..'^0')
+                print('Novinky: ^2'..(data.news or 'Žádné info')..'^0')
+                print('^5Stahujte na: https://github.com/Mathewgamingg/mg_codestashes^0')
+                print(symbols)
             end
-            symbols = symbols..'^0'
-            print(symbols)
-            print('^3['..resourceName..'] - New update available now!^0\nCurrent Version: ^1'..currentVersion..'^0.\nNew Version: ^2'..result.version..'^0.\nNews: ^2'..result.news..'^0.\n\n^5Download it now on your keymaster.fivem.net^0.')
-            print(symbols)
+        else
+            print('^1['..resourceName..'] Nepodařilo se zpracovat data o verzi (neplatný formát JSON).^0')
         end
     end, 'GET')
 end)
